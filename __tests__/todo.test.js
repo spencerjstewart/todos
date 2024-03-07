76d21acf85ada1fcb62b88846238dbd7cf19b43c
Spencer Stewart <37820157+spencerjstewart@users.noreply.github.com>
2024-03-07 15:11:31 -0600

    test: add jest, implement tests for CRUD functions, encapsulate functionality in a class, and fix toggle bug (#15)
    * build: add jest
    
    * test: add test script to run jest
    
    * test: install types for jest
    
    * test: create test for add function
    
    * feat: encapsulate current functionality in a class
    
    * feat: update constructor to optionally take an array of todos
    
    * test: update test to operate on a TodoList instance
    
    * test: add test for edit function
    
    * test: add test for remove function
    
    * test: add test for toggle function
    
    * fix: properly flip the completed property on a todo

---
+++ __tests__/todo.test.js
@@ -0,0 +1,47 @@
+const {TodoList} = require('../index');
+require('jest')
+
+const initTodos = () => {
+  return new TodoList([{
+    todoText: 'Drink water', completed: false
+  }, {
+    todoText: 'Go for a walk', completed: false
+  }, {
+    todoText: 'Learn JavaScript', completed: false
+  }])
+}
+
+describe('Todo App', () => {
+  describe('The function', () => {
+    let todoList;
+
+    beforeEach(() => {
+      todoList = initTodos()
+    })
+
+    test('add should add objects to todos.', () => {
+      todoList.add('Clean emails')
+      expect(todoList.todos[3].todoText).toBe('Clean emails')
+    })
+
+    test('edit should accept an index and some todo text and edit the todos' + ' list in-place', () => {
+      todoList.edit(1, 'edited todo')
+      expect(todoList.todos[0].todoText).toBe('Drink water')
+      expect(todoList.todos[1].todoText).toBe('edited todo')
+      expect(todoList.todos[2].todoText).toBe('Learn JavaScript')
+    })
+
+    test('remove should remove a todo in-place', () => {
+      todoList.remove(1)
+      expect(todoList.todos[0].todoText).toBe('Drink water')
+      expect(todoList.todos[1].todoText).toBe('Learn JavaScript')
+    })
+
+    test('toggle should flip the completed property on a todo', () => {
+      todoList.toggle(0)
+      expect(todoList.todos[0].completed).toBe(true)
+      todoList.toggle(0)
+      expect(todoList.todos[0].completed).toBe(false)
+    })
+  })
+})
--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const todos = []
+class TodoList {
+  constructor(todos = []) {
+    if (Array.isArray(todos) && todos.length > 0 && todos.every(todo => typeof todo === 'object' && 'todoText' in todo && 'completed' in todo)) {
+      this.todos = todos;
+    } else {
+      this.todos = [];
+    }
+  }
 
-const displayTodos = () => {
-  console.log(todos)
+  displayTodos = () => {
+    console.log(this.todos)
+  }
+
+  add = (todoText) => {
+    this.todos.push({
+      todoText, completed: false
+    })
+    this.displayTodos()
+  }
+
+  edit = (index, todoText) => {
+    this.todos[index].todoText = todoText
+    this.displayTodos()
+  }
+
+  remove = (index) => {
+    this.todos.splice(index, 1)
+    this.displayTodos()
+  }
+
+  toggle = (index) => {
+    this.todos[index].completed = !this.todos[index].completed
+    this.displayTodos()
+  }
 }
 
-const add = (todoText) => {
-  todos.push({
-    todoText,
-    completed: false
-  })
-  displayTodos()
-}
-
-const edit = (index, todoText) => {
-  todos[index].todoText = todoText
-  displayTodos()
-}
-
-const remove = (index) => {
-  todos.splice(index, 1)
-  displayTodos()
-}
-
-const toggle = (index) => {
-  todos[index].completed = !completed
-  displayTodos()
+module.exports = {
+  TodoList
 }
