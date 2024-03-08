781809a39917826b8106b690faff7669e5fd5374
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-03-08 17:37:30 -0600

    refactor: move TodoList class into its own file

--- js/index.js
+++ js/index.js
@@ -1,73 +1,8 @@
-class TodoList {
-  constructor(todos = []) {
-    if (
-      Array.isArray(todos) &&
-      todos.length > 0 &&
-      todos.every(
-        (todo) =>
-          typeof todo === "object" && "todoText" in todo && "completed" in todo,
-      )
-    ) {
-      this.todos = todos;
-    } else {
-      this.todos = [];
-    }
-  }
+const { TodoList } = require("./todoList");
+const { TodoListUI } = require("./todoListUI");
 
-  displayTodos = () => {
-    this.todos.forEach((todo) => {
-      console.log(`${todo.completed ? "[X]" : "[]"} ${todo.todoText}`);
-    });
-  };
-
-  add = (todoText) => {
-    this.todos.push({
-      todoText,
-      completed: false,
-    });
-    this.displayTodos();
-  };
-
-  edit = (index, todoText) => {
-    this.todos[index].todoText = todoText;
-    this.displayTodos();
-  };
-
-  remove = (index) => {
-    this.todos.splice(index, 1);
-    this.displayTodos();
-  };
-
-  toggle = (index) => {
-    this.todos[index].completed = !this.todos[index].completed;
-    this.displayTodos();
-  };
-
-  toggleAll = () => {
-    let numTrue = 0;
-
-    this.todos.forEach((todo) => {
-      if (todo.completed) {
-        numTrue += 1;
-      }
-    });
-
-    if (numTrue > 0 && numTrue !== this.todos.length) {
-      this.todos.forEach((todo) => {
-        todo.completed = true;
-      });
-    }
-
-    if (numTrue === this.todos.length) {
-      this.todos.forEach((todo) => {
-        todo.completed = false;
-      });
-    }
-
-    this.displayTodos();
-  };
-}
-
-module.exports = {
-  TodoList,
-};
+(function init() {
+  const todoList = new TodoList();
+  const ui = new TodoListUI(todoList);
+  ui.init();
+})();
---
+++ js/todoList.js
@@ -0,0 +1,73 @@
+class TodoList {
+  constructor(todos = []) {
+    if (
+      Array.isArray(todos) &&
+      todos.length > 0 &&
+      todos.every(
+        (todo) =>
+          typeof todo === "object" && "todoText" in todo && "completed" in todo,
+      )
+    ) {
+      this.todos = todos;
+    } else {
+      this.todos = [];
+    }
+  }
+
+  displayTodos = () => {
+    this.todos.forEach((todo) => {
+      console.log(`${todo.completed ? "[X]" : "[]"} ${todo.todoText}`);
+    });
+  };
+
+  add = (todoText) => {
+    this.todos.push({
+      todoText,
+      completed: false,
+    });
+    this.displayTodos();
+  };
+
+  edit = (index, todoText) => {
+    this.todos[index].todoText = todoText;
+    this.displayTodos();
+  };
+
+  remove = (index) => {
+    this.todos.splice(index, 1);
+    this.displayTodos();
+  };
+
+  toggle = (index) => {
+    this.todos[index].completed = !this.todos[index].completed;
+    this.displayTodos();
+  };
+
+  toggleAll = () => {
+    let numTrue = 0;
+
+    this.todos.forEach((todo) => {
+      if (todo.completed) {
+        numTrue += 1;
+      }
+    });
+
+    if (numTrue > 0 && numTrue !== this.todos.length) {
+      this.todos.forEach((todo) => {
+        todo.completed = true;
+      });
+    }
+
+    if (numTrue === this.todos.length) {
+      this.todos.forEach((todo) => {
+        todo.completed = false;
+      });
+    }
+
+    this.displayTodos();
+  };
+}
+
+module.exports = {
+  TodoList,
+};
