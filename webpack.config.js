e5a08fdefb04c8f7c62d61a61fa98056a482588d
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-03-09 22:40:31 -0600

    chore: configure devServer and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
-// webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./js/index.js", // Update this if your entry file is different
+  entry: "./js/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  mode: "development", // Use 'production' for production build
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    open: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
