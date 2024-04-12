9a1d5017b560876ae8731075e3d6735f53f41099
Spencer Stewart <37820157+spencerjstewart@users.noreply.github.com>
2024-04-11 20:21:42 -0500

    fix: reconfigure webpack to automatically refresh browser on file change (#59)
    * fix: fix hot reloading setup for development environment
    
    * chore: remove unnecessary requires

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
 const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require("webpack-hot-middleware");
-const webpackConfig = require("./webpack.config");
 const { join } = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV === "development") {
-  const compiler = webpack(webpackConfig);
-  app.use(
-    webpackDevMiddleware(compiler, {
-      publicPath: webpackConfig.output.publicPath,
-    }),
-  );
-  app.use(webpackHotMiddleware(compiler));
-}
 
 app.use(express.static("dist"));
 
